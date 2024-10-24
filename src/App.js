974c1af6120dfd6dec6335fd4a70f37a7dcf6bd0
FilipeFernandesAbase <filipe.fernandes@abase.com.br>
2024-10-24 14:31:12 -0300

    Teste

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ptBR from 'antd/lib/locale/pt_BR';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import { Login } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider locale={ptBR}>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </ConfigProvider>
   );
 }
 
---
+++ src/pages/login/index.js
@@ -0,0 +1,18 @@
+import { Col, Layout, Row } from 'antd';
+import React, { useEffect, useState } from 'react';
+
+export default function Login() {
+
+    return (
+        <Layout>
+            <Layout.Content>
+                <Row justify="center" >
+                    <Col span={16}>
+                        aaaaaaaaaaaaaaaaaaaaaaaaTESTE
+
+                    </Col>
+                </Row>
+            </Layout.Content>
+        </Layout>
+    );
+}
